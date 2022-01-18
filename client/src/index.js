44099473910946d41e34bd552059338fae6b3ad8
Nuccino92 <anthnucci@hotmail.com>
2022-01-18 18:38:24 -0500

    added redux provider w/ store

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import "./index.css";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// reducers index export
+import reducers from "./reducers";
+
+// create store, give store reducers, apply thunk middleware
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  // wrap provider around app and give store to everything in app
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
