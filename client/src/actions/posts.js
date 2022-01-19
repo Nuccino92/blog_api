5c6bcef7fc3e3ebe39754dcd16f63ecc6a564c75
Nuccino92 <anthnucci@hotmail.com>
2022-01-19 18:11:41 -0500

    added post action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,20 @@
-import * as api from "../api";
+import { fetchPosts, createPosts } from "../api/posts";
 
 export const getPosts = () => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    const response = await fetchPosts();
 
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: "FETCH_ALL", payload: response.data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const postPosts = (postData) => async (dispatch) => {
+  try {
+    const response = await createPosts(postData);
+
+    dispatch({ type: "CREATE", payload: response.data });
   } catch (err) {
     console.log(err.message);
   }
