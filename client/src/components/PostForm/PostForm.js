e191762cf0158e2a87c1ae9757a9d1d4d9d59fe5
Nuccino92 <anthnucci@hotmail.com>
2022-01-19 18:12:23 -0500

    added form for post requests

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { postPosts } from "../../actions/posts";
+
 const PostForm = () => {
+  const dispatch = useDispatch();
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    published: undefined,
+    author: "anthony",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(postPosts(postData));
+  };
+
   return (
     <div>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title of Blog Post</label>
+        <input
+          name="title"
+          type="text"
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        ></input>
+        <label htmlFor="content">Content</label>
+        <textarea
+          name="title"
+          type="text"
+          value={postData.content}
+          onChange={(e) =>
+            setPostData({ ...postData, content: e.target.value })
+          }
+        ></textarea>
+
+        <fieldset>
+          {" "}
+          <legend>Would you like to publish this blog?</legend>
+          <input
+            id="publishedTrue"
+            type="radio"
+            name="published"
+            onChange={(e) => setPostData({ ...postData, published: true })}
+          />
+          <label htmlFor="publishedTrue">Yes</label>
+          <input
+            id="publishedFalse"
+            type="radio"
+            name="published"
+            onChange={(e) => setPostData({ ...postData, published: false })}
+          />
+          <label htmlFor="publishedFalse">No</label>
+        </fieldset>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
