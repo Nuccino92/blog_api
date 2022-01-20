2f04304c42435e03d5d8ad2e41454bc635f28cbe
Nuccino92 <anthnucci@hotmail.com>
2022-01-20 01:09:04 -0500

    renders published posts w/ loading spinner

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,20 @@
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { TailSpin } from "react-loader-spinner";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 
 const Posts = () => {
-  // grab state from the post reducer
-  const posts = useSelector((state) => state.postReducer);
+  // grab state from postReducer in the store
+  const posts = useSelector((reducers) => reducers.postReducer);
 
-  return (
-    <div>
+  return posts.length > 0 ? (
+    <div className="posts">
       {posts.map((post, index) => {
-        return <Post info={post} key={index} />;
+        return post.published && <Post info={post} key={index} />;
       })}
     </div>
+  ) : (
+    <TailSpin color="blue" />
   );
 };
 
