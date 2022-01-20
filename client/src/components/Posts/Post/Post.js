594d479a034e884753626cadad3c0fee37462f17
Nuccino92 <anthnucci@hotmail.com>
2022-01-20 01:09:28 -0500

    post component using state

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,15 @@
 const Post = ({ info }) => {
+  const { title, content, author } = info;
   console.log(info);
-  return <div>POST</div>;
+  return (
+    <div className="post">
+      <h3 className="post-title">{title}</h3>
+      <p>{content}</p>
+      <div>{author}</div>
+      <button>EDIT</button>
+      <button>DELETE</button>
+    </div>
+  );
 };
 
 export default Post;
