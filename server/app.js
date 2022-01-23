36cf39e014ba30ce7ede48d63521b98da4a35a28
Nuccino92 <anthnucci@hotmail.com>
2022-01-22 19:13:45 -0500

    created call to check if user is logged in

--- server/app.js
+++ server/app.js
@@ -5,8 +5,76 @@ import passport from "passport";
 import cors from "cors";
 
 import postsRoutes from "./routes/posts.js";
+import registerRoutes from "./routes/register.js";
+import logInRoutes from "./routes/log-in.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
+
+import passportLocal from "passport-local";
+import passportJWT from "passport-jwt";
+
+import User from "./models/user.js";
+
+import { compareHash } from "./utils/passwords.js";
+
+const LocalStrategy = passportLocal.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+dotenv.config();
+const dbURI = process.env.MONGOOSE_URI;
+const PORT = process.env.PORT || 8000;
+const secretKey = process.env.JWT_SECRET;
+
+app.use(passport.initialize());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async function (email, password, cb) {
+      return await User.findOne({ email: new RegExp("^" + email + "$", "i") })
+        .then((user) => {
+          if (!user) {
+            return cb(null, false, {
+              message: "Incorrect email or password",
+            });
+          }
+          if (!compareHash(password, user.password)) {
+            return cb(null, false, {
+              message: "Incorrect email or password",
+            });
+          }
+          return cb(null, user, { message: "Logged In Successfully" });
+        })
+        .catch((err) => {
+          cb(err), console.log(err);
+        });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secretKey,
+    },
+    async (jwtPayload, cb) => {
+      return await User.findById(jwtPayload.id)
+        .then((user) => {
+          if (user) {
+            return cb(null, user);
+          }
+          return cb(null, false);
+        })
+        .catch((err) => console.log(err));
+    }
+  )
+);
 
 // handles front end request body
 app.use(express.json());
@@ -14,10 +82,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-
-dotenv.config();
-const dbURI = process.env.MONGOOSE_URI;
-const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -30,3 +94,6 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 app.use("/posts", postsRoutes);
+app.use("/register", registerRoutes);
+app.use("/log-in", logInRoutes);
+app.use("/auth", authRoutes);
---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+import User from "../models/user.js";
+
+export const auth_Get = (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then((user) => {
+      res.json(user);
+    });
+};
