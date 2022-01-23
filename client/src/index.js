4b207d5cdb12beb77640e7cb55f4a8a2d823485a
Nuccino92 <anthnucci@hotmail.com>
2022-01-22 19:12:02 -0500

    redux chrome devtools

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import "./index.css";
 
 import App from "./App";
 
-// reducers index export
+// reducers index, export default
 import reducers from "./reducers";
 
 // create store, give store reducers, apply thunk middleware
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   // wrap provider around app and give store to everything in app
