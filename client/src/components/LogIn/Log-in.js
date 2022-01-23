620e5e00e41f361b3bf5c530977b28d8e8732b8f
Nuccino92 <anthnucci@hotmail.com>
2022-01-22 19:19:09 -0500

    added form to component

--- client/src/components/LogIn/Log-in.js
+++ client/src/components/LogIn/Log-in.js
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import { logInRequest } from "../../api/log-in";
+
 const LogIn = () => {
-  return <div>login</div>;
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    logInRequest(userData);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          value={userData.username}
+          onChange={(e) =>
+            setUserData({ ...userData, username: e.target.value })
+          }
+        ></input>
+        <label htmlFor="password">password</label>
+        <input
+          type="text"
+          value={userData.password}
+          onChange={(e) =>
+            setUserData({ ...userData, password: e.target.value })
+          }
+        ></input>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 export default LogIn;
