929361d3f7470d38e61dd44eaef416a67ddab51f
Nuccino92 <anthnucci@hotmail.com>
2022-01-24 17:28:47 -0500

    created types

---
+++ client/src/actions/types.js
@@ -0,0 +1,15 @@
+// posts
+export const FETCH_ALL = "FETCH_ALL";
+export const CREATE_POST = "CREATE_POST";
+// user
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const AUTH_ERR = " AUTH_ERR ";
+// errors
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
