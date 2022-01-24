1cbdb641d8e548c881bed831fcba6cf2ecfd9bce
Nuccino92 <anthnucci@hotmail.com>
2022-01-24 17:32:18 -0500

    added 2 more reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 
 import postReducer from "./posts";
+import userReducer from "./user";
+import errorReducer from "./error";
 
 // calls every child reducer and gathers their results into a single state object
 export default combineReducers({
   postReducer,
+  userReducer,
+  errorReducer,
 });
