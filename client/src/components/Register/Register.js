1a4a9fc6273684cf49740558aaeca919f6e10a58
Nuccino92 <anthnucci@hotmail.com>
2022-01-24 17:31:30 -0500

    register form w/use of state & router navigate

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,4 +1,73 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors } from "../../actions/error";
+import { createUser } from "../../actions/user";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
-  return <div>this is the register</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const states = useSelector((state) => state.errorReducer);
+  // if register_fail set error message, if register_success navigate to home
+  useEffect(() => {
+    if (states.id === "REGISTER_FAIL") {
+      setErrorMessage(states.message.message);
+    } else {
+      setErrorMessage(null);
+    }
+    if (states.id === "REGISTER_SUCCESS") {
+      navigate("/");
+    }
+  }, [states, errorMessage, navigate]);
+
+  // resets errors on page load
+  useEffect(() => {
+    dispatch(clearErrors());
+  }, [dispatch]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createUser(userData));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          type="text"
+          value={userData.email}
+          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        ></input>
+        {errorMessage === "Email already taken" && <div>{errorMessage}</div>}
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          value={userData.username}
+          onChange={(e) =>
+            setUserData({ ...userData, username: e.target.value })
+          }
+        ></input>
+        {errorMessage === "Username already taken" && <div>{errorMessage}</div>}
+        <label htmlFor="password">password</label>
+        <input
+          type="text"
+          value={userData.password}
+          onChange={(e) =>
+            setUserData({ ...userData, password: e.target.value })
+          }
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      {errorMessage === "Please fill out form" && <div>{errorMessage}</div>}
+    </div>
+  );
 };
 export default Register;
