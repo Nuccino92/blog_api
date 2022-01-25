0f71eebe1eb9b4016fc32b09bdf8752048b6ed5c
Nuccino92 <anthnucci@hotmail.com>
2022-01-24 22:01:49 -0500

    handles token config

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const url = "http://localhost:8000/posts";
 
 export const fetchPostsRequest = () => axios.get(url);
-export const createPostsRequest = (postData) => axios.post(url, postData);
-
+export const createPostsRequest = (postData, headers) =>
+  axios.post(url, postData, headers);
