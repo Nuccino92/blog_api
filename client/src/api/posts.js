69e0544390a64b2d8ae2548f93d12937221c50aa
Nuccino92 <anthnucci@hotmail.com>
2022-01-26 01:11:33 -0500

    edited post requests

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -5,3 +5,8 @@ const url = "http://localhost:8000/posts";
 export const fetchPostsRequest = () => axios.get(url);
 export const createPostsRequest = (postData, headers) =>
   axios.post(url, postData, headers);
+
+export const deletePostRequest = (id, headers) =>
+  axios.delete(`${url}/${id}`, headers);
+
+export const updatePostRequest = (data) => axios.put(url, data);
