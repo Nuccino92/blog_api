8f5f90d8900553e181731b50a5c1f169664d7f69
Nuccino92 <anthnucci@hotmail.com>
2022-01-26 01:11:18 -0500

    added update/delete post types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,8 @@
 // posts
 export const FETCH_ALL = "FETCH_ALL";
 export const CREATE_POST = "CREATE_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UPDATE_POST = "UPDATE_POST";
 // user
 export const USER_LOADING = "USER_LOADING";
 export const USER_LOADED = "USER_LOADED";
