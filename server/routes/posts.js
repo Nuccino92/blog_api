9a75d291f58a40c539817e9f75455db19d388bb2
Nuccino92 <anthnucci@hotmail.com>
2022-01-26 01:16:06 -0500

    delete post route with jwt auth

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,6 +5,7 @@ import {
   getPosts_Index,
   createPost_Post,
   editPost_Put,
+  deletePost_Delete,
 } from "../controllers/posts.js";
 
 const router = express.Router();
@@ -14,6 +15,11 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   createPost_Post
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  deletePost_Delete
 );
 router.put("/:id", editPost_Put);
 
