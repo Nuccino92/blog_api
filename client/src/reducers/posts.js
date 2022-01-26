449f6710a4b86722436a6da21dd4514911dc26a2
Nuccino92 <anthnucci@hotmail.com>
2022-01-26 01:13:03 -0500

    refactor/ added delete post

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,31 @@
+import {
+  FETCH_ALL,
+  CREATE_POST,
+  DELETE_POST,
+  UPDATE_POST,
+} from "../actions/types";
+
+const initialState = {
+  posts: [],
+};
+
 // make state's initial state an empty array
-const postReducer = (state = [], action) => {
+const postReducer = (state = initialState, action) => {
   // reducer looks at action type field to decide what happens
   switch (action.type) {
     // do something here based on different types of actions
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE_POST":
+    case CREATE_POST:
       // pushing payload(created post) to end of state
-      return [...state, action.payload];
+      return [...state.posts, action.payload];
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
+    case UPDATE_POST:
+      return state;
     default:
       return state;
   }
