581d47a0c39550a43fa43a58777f703f05f53f47
Nuccino92 <anthnucci@hotmail.com>
2022-01-26 01:12:34 -0500

    added delete button

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,24 @@
+import { useDispatch } from "react-redux";
+// import { useLocation } from "react-router-dom";
+import { deletePost } from "../../../actions/posts";
+
 const Post = ({ info }) => {
+  const dispatch = useDispatch();
+  // const state = useLocation();
   const { title, content, author } = info;
-  console.log(info);
+  const id = info._id;
+
+  const handleDelete = () => {
+    dispatch(deletePost(id));
+  };
+
   return (
     <div className="post">
       <h3 className="post-title">{title}</h3>
       <p>{content}</p>
       <div>{author}</div>
       <button>EDIT</button>
-      <button>DELETE</button>
+      <button onClick={handleDelete}>DELETE</button>
     </div>
   );
 };
